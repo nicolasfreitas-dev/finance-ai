18450592850859cd96a44c45be12eef0808d0b05
Nicolas <nicolasfpdev@gmail.com>
2024-11-06 16:00:21 -0300

    feat: create login page

---
+++ app/login/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { Button } from "../_components/ui/button";
+import { LogInIcon } from "lucide-react";
+
+const LoginPage = () => {
+    return ( 
+        <div className="h-full grid grid-cols-2">
+            <div className="flex h-full flex-col justify-center p-8 max-w-[550px] mx-auto">
+                <Image src="/logo.svg" alt="Logo Finance AI" width={173} height={89} className="mb-8" />
+                <h1 className="text-4xl font-bold mb-3">Bem vindo</h1>
+                <p className="text-muted-foreground mb-8">A Finance AI é uma plataforma de gestão financeira que utiliza IA para monitorar suas movimentações, e oferecer insights personalizados, facilitando o controle do seu orçamento.</p>
+                <Button variant="outline">
+                    <LogInIcon className="mr-2"/>
+                    Fazer login ou criar conta
+                </Button>
+            </div>
+            <div className="relative h-full w-full">
+                <Image src="/login.png" alt="Faça login" fill className="object-cover"/>
+            </div>
+        </div>
+     );
+}
+ 
+export default LoginPage;
